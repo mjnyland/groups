f166411d860a1c9a6b08433d592a3bf5978e6f7d
Michael <88445485+mjnyland@users.noreply.github.com>
2025-01-30 20:42:52 -0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -19,30 +19,46 @@ function App() {
 function Main() {
   const location = useLocation(); // Get the current route location
 
+  const handleClearLocalStorage = () => {
+    localStorage.clear();
+    window.location.reload(); // Clear all data in local storage
+  };
+
   return (
     <div className="min-h-screen bg-gray-900 flex gap-[16px] justify-center p-6 w-full">
       <div className="flex flex-col gap-[16px] w-full">
-        <div className="flex gap-[8px]">
-          <Link
-            to="/option-one"
-            className={`text-white text-lg px-4 py-2 rounded-lg ${
-              location.pathname === "/option-one"
-                ? "bg-gray-100"
-                : "bg-gray-800"
-            }`}
-          >
-            People in many groups
-          </Link>
-          <Link
-            to="/option-two"
-            className={`text-white text-lg px-4 py-2 rounded-lg ${
-              location.pathname === "/option-two"
-                ? "bg-gray-100"
-                : "bg-gray-800"
-            }`}
-          >
-            People in one group
-          </Link>
+        <div className="flex justify-between">
+          <div className="flex gap-[8px]">
+            <Link
+              to="/option-one"
+              className={`text-white text-lg px-4 py-2 rounded-lg ${
+                location.pathname === "/option-one"
+                  ? "bg-gray-100"
+                  : "bg-gray-800"
+              }`}
+            >
+              People in many groups
+            </Link>
+            <Link
+              to="/option-two"
+              className={`text-white text-lg px-4 py-2 rounded-lg ${
+                location.pathname === "/option-two"
+                  ? "bg-gray-100"
+                  : "bg-gray-800"
+              }`}
+            >
+              People in one group
+            </Link>
+          </div>
+          {/* Clear Local Storage Button */}
+          <div className="">
+            <button
+              onClick={handleClearLocalStorage}
+              className="bg-red-500 text-white text-lg px-4 py-2 rounded-lg hover:bg-red-600 transition"
+            >
+              Clear data
+            </button>
+          </div>
         </div>
 
         <Routes>
